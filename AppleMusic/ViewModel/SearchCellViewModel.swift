1a605903abdd468bb7d7e6f396a878b18b7e2525
Halil Yavuz <halil9913@gmail.com>
2024-02-19 15:32:02 +0300

    v 1.0

--- AppleMusic/ViewModel/SearchCellViewModel.swift
+++ AppleMusic/ViewModel/SearchCellViewModel.swift
@@ -8,18 +8,19 @@
 import Foundation
 
 struct SearchCellViewModel {
-    struct Cell: TrackCellViewModel {
-        var trackName: String
-        
-        var artistName: String
-        
-        var collectionName: String
-        
-        var iconUrlString: String?
-        
-        var trackURL: String
-
-    }
+    
     var cells: [Cell]
+}
+struct Cell: TrackCellViewModel {
+    var trackName: String
+    
+    var artistName: String
+    
+    var collectionName: String
+    
+    var iconUrlString: String?
+    
+    var trackURL: String
+
 }
 
